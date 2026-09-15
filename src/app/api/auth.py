fbcf316cd8ce1e4fdd21f23be9fba05e39d9d198
Paata Sivsivadze <paatasivsivadze1@gmail.com>
2026-09-15 15:42:34 +0400

    finished-auth

--- src/app/api/auth.py
+++ src/app/api/auth.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
+from app.api.dependencies import LoginForm,AuthService
+from app.schemas.api.token import Token
 
 router = APIRouter()
 
 
 router.post('login')
+
+
+@router.post('/login',response_model=Token)
+async def login(auth_ser: AuthService,form_data : LoginForm):
+    username , password = form_data.username,form_data.password
+    jwt_token = await auth_ser.login_for_access_token(username, password)
+
+    return Token(access_token=jwt_token,token_type="bearer")
